17154e8c9caa1f382384ce15663f9a00f0400bf5
Linh Nguyễn <linhyun23091999@gmail.com>
2020-04-22 17:21:28 +0700

    Baitap6_chuong3_part1

---
+++ Baitap6_chuong3_part1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Baitap6_chuong3_part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
